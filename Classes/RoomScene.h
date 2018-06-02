89d314af51364ed71a15ac76510f8a029f5d3e6c
zelotuo <37100615+yaya159177@users.noreply.github.com>
2018-06-02 18:53:05 +0800

    @liuze

--- Classes/RoomScene.h
+++ Classes/RoomScene.h
@@ -1,11 +1,19 @@
 #ifndef __ROOMSCENE_H__
 #define __ROOMSCENE_H__
 
+#include<vector>
+#include<iostream>
 #include "cocos2d.h"
+#include"ui/CocosGUI.h"
+#include"cocos-ext.h"
 
 USING_NS_CC;
+USING_NS_CC_EXT;
 
-class RoomScene:public cocos2d::Layer
+using namespace std;
+using namespace cocos2d::ui;
+
+class RoomScene:public cocos2d::Layer，public EditBoxDelegate,public ScrollViewDelegate
 {
 public:
 	static Scene* createScene();
@@ -17,6 +25,26 @@ public:
 
 	void CountryButtonCallback(cocos2d::Ref* pSender);
 
+	//begin
+	virtual void onEnter();
+	virtual void onExit();
+    virtual bool init();
+    
+	vector<int> hides = { 2,3,5 };
+	vector<int> show = { 1 };
+	Vec2 chosen_position = { 500,500 };
+
+	bool touchBegin(Touch* touch, Event* event);
+	void touchMoved(Touch* touch, Event* event);
+	void touchEnded(Touch* touch, Event* event);
+	//Slider滑动事件回调函数
+	void onChangedSlider(Ref* pSender, Slider::EventType type);
+    
+	//end
+	virtual void editBoxEditingDidBegin(cocos2d::extension::EditBox* editBox);
+	virtual void editBoxEditingDidEnd(cocos2d::extension::EditBox* editBox);
+	virtual void editBoxTextChanged(cocos2d::extension::EditBox* editBox, const std::string &text);
+	virtual void editBoxReturn(cocos2d::extension::EditBox *editBox);
 
 	CREATE_FUNC(RoomScene);
 };
