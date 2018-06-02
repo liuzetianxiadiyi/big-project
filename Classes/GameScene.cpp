cfae0d168e5e90dcd42cc629e80d8a6882f494be
zelotuo <37100615+yaya159177@users.noreply.github.com>
2018-06-02 12:14:25 +0800

    @liuze mouse

--- Classes/GameScene.cpp
+++ Classes/GameScene.cpp
@@ -22,7 +22,7 @@ bool GameScene::init()
 
 	_tileMap = TMXTiledMap::create(".tmx");
 	addChild(_tileMap, 0);
-	//´ýµ÷Õû
+	//Â´Ã½ÂµÃ·Ã•Ã»
 	TMXObjectGroup* group = _tileMap->getObjectGroup("objects");
 	ValueMap spawnPoint = group->getObject("born");
 
@@ -36,7 +36,56 @@ bool GameScene::init()
 	_collidable = _tileMap->getLayer("collodable");
 	_collidable->setVisible(false);
 
+	//mouse chooose
+
+	auto ml = EventListenerTouchOneByOne::create();
+	ml->onTouchBegan = CC_CALLBACK_2(HelloWorld::MouseDown, this);
+	ml->onTouchMoved = CC_CALLBACK_2(HelloWorld::MouseMoved, this);
+	ml->onTouchEnded = CC_CALLBACK_2(HelloWorld::MouseUp, this);
+	_eventDispatcher->addEventListenerWithSceneGraphPriority(ml,this);
+	
+	auto s = Sprite::create("s.png");
+	s->setPosition(Vec2(0, 0));
+	s->setTag(10);
+	s->setAnchorPoint(Vec2(0, 0));
+	this->addChild(s,10);
+
+	//end
+	
 	return true;
+}
+
+//mouse choose
+
+bool HelloWorld::MouseDown(Touch *touch, Event *unused_event)
+{
+	auto e = touch;
+	Vec2 pos = e->getLocation();
+	auto s = getChildByTag(10);
+	s->setPosition(pos);
+
+	return true;
+}
+
+void HelloWorld::MouseMoved(Touch *touch, Event *unused_event)
+{
+	auto e = touch;
+	auto s = getChildByTag(10);
+	Vec2 pos = e->getDelta() - s->getPosition();
+	if (pos.x < 0)
+		pos.x = -pos.x;
+	if (pos.y < 0)
+		pos.y = -pos.y;
+	int x = pos.x;
+	int y = pos.y;
+	s->runAction(ScaleTo::create(0.1, x, y));
+	std::cout << x << std::endl;
+	std::cout << y << std::endl;
+}
+
+void HelloWorld::MouseUp(Touch *touch, Event *unused_event)
+{
+	
 }
 
 void GameScene::setPlayerPosition(Vec2(position))
@@ -60,7 +109,7 @@ void GameScene::setPlayerPosition(Vec2(position))
 	}
 	for (auto s : sVector)
 	{
-		//Ã¿¸ö¾«ÁéµÄÒÆ¶¯
+		//ÃƒÂ¿Â¸Ã¶Â¾Â«ÃÃ©ÂµÃ„Ã’Ã†Â¶Â¯
 	}
 }
 
