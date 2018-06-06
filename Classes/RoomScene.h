c8501ab8661f5b9a8ff4018c19fb995b556413a8
zelotuo <37100615+yaya159177@users.noreply.github.com>
2018-06-06 23:02:38 +0800

    @liuze 国家

--- Classes/RoomScene.h
+++ Classes/RoomScene.h
@@ -2,7 +2,6 @@
 #define __ROOMSCENE_H__
 
 #include<vector>
-#include<iostream>
 #include "cocos2d.h"
 #include"ui/CocosGUI.h"
 #include"cocos-ext.h"
@@ -22,25 +21,15 @@ public:
 
 	void menuStartGameCallback(cocos2d::Ref* pSender);
 	void menuDeleteRoomCallback(cocos2d::Ref* pSender);
+	void CountryButtonCallback(cocos2d::Ref* pSender);	
+    	
+	//国家的button
+	vector<Button*> hides;
+	vector<Button*> show;
 
-	void CountryButtonCallback(cocos2d::Ref* pSender);
-
-	//begin
-	virtual void onEnter();
-	virtual void onExit();
-    virtual bool init();
-    
-	vector<int> hides = { 2,3,5 };
-	vector<int> show = { 1 };
-	Vec2 chosen_position = { 500,500 };
-
-	bool touchBegin(Touch* touch, Event* event);
-	void touchMoved(Touch* touch, Event* event);
-	void touchEnded(Touch* touch, Event* event);
-	//Slider滑动事件回调函数
-	void onChangedSlider(Ref* pSender, Slider::EventType type);
-    
-	//end
+	//国家回调函数
+	void countryCallBack(Ref* pSendre, Widget::TouchEventType type);
+	
 	virtual void editBoxEditingDidBegin(cocos2d::extension::EditBox* editBox);
 	virtual void editBoxEditingDidEnd(cocos2d::extension::EditBox* editBox);
 	virtual void editBoxTextChanged(cocos2d::extension::EditBox* editBox, const std::string &text);
