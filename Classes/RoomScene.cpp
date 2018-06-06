9627e6234b9611b88078d908aa6fa13b2c9dacce
zelotuo <37100615+yaya159177@users.noreply.github.com>
2018-06-06 23:10:50 +0800

    @liuze 选国家

--- Classes/RoomScene.cpp
+++ Classes/RoomScene.cpp
@@ -24,30 +24,6 @@ Scene* RoomScene::createScene()
 	return scene;
 }
 
-void RoomScene::onEnter()
-{
-	Layer::onEnter();
-	//
-	auto listener = EventListenerTouchOneByOne::create();
-	//listener->setSwallowTouches(true);
-	listener->onTouchBegan = CC_CALLBACK_2(RoomScene::touchBegin, this);
-	listener->onTouchMoved = CC_CALLBACK_2(RoomScene::touchMoved, this);
-	listener->onTouchEnded = CC_CALLBACK_2(RoomScene::touchEnded, this);
-
-	auto eventdis = Director::getInstance()->getEventDispatcher();
-	eventdis->addEventListenerWithSceneGraphPriority(listener, getChildByTag(1));
-	eventdis->addEventListenerWithSceneGraphPriority(listener->clone(), getChildByTag(2));
-	eventdis->addEventListenerWithSceneGraphPriority(listener->clone(), getChildByTag(3));
-	eventdis->addEventListenerWithSceneGraphPriority(listener->clone(), getChildByTag(4));
-
-}
-
-void RoomScene::onExit()
-{
-	Layer::onExit();
-
-}
-
 bool RoomScene::init()
 {
 	if (!Layer::init())
@@ -92,48 +68,40 @@ bool RoomScene::init()
 	Accont_back->setPosition(Vec2(origin.x + visibleSize.width / 2, origin.y + visibleSize.height / 2));
 	this->addChild(Accont_back, 0);
 	
-	//创建滑块控件
-	Slider* slider = Slider::create();
-	//加载滑杆纹理
-	slider->loadBarTexture("sliderTrack.png");
-	//加载滑块按钮纹理
-	slider->loadSlidBallTextures("sliderThumb.png", "sliderThumb.png", "");
-	//加载滑块进度栏纹理
-	slider->loadProgressBarTexture("sliderProgress.png");
-	//The max percent of Slider.
-	slider->setMaxPercent(100);
-	slider->setRotation(90);
-	slider->setTag(5);
-	slider->setPosition(Vec2(visibleSize.width / 2.0f + 60, visibleSize.height / 2.0f));
-	slider->addEventListener(CC_CALLBACK_2(RoomScene::onChangedSlider, this));
-	
-	this->addChild(slider, 10);
-
-	auto china = Sprite::create("china.png");
-	china->setPosition(Vec2(500, 500));
-	china->setTag(1);
+	//选国家
+	auto china = Button::create("china.png");
+	china->setPosition(Vec2(visibleSize.width / 2.0f, visibleSize.height / 2.0f));
+	show.push_back(china);
+	china->addTouchEventListener(CC_CALLBACK_2(RoomScene::countryCallBack, this));
 	this->addChild(china, 10);
 
-	auto us = Sprite::create("us.png");
-	us->setPosition(Vec2(500, 500 - 30 - china->getContentSize().height));
+	//国家button的间隔
+	float country_interval = china->getContentSize().height/10;
+
+	auto us = Button::create("america.png");
+	us->setPosition(Vec2(visibleSize.width / 2.0f , visibleSize.height / 2.0f - country_interval - china->getContentSize().height));
 	us->setTag(2);
+	hides.push_back(us);
+	us->addTouchEventListener(CC_CALLBACK_2(RoomScene::countryCallBack, this));
 	this->addChild(us, 10);
 
-	auto c1 = Sprite::create("china.png");
-	c1->setPosition(Vec2(500, 500 - 30 * 2 - china->getContentSize().height * 2));
-	c1->setTag(3);
-	this->addChild(c1, 10);
+	auto japan = Button::create("japan.png");
+	japan->setPosition(Vec2(visibleSize.width / 2.0f, visibleSize.height / 2.0f - country_interval * 2 - china->getContentSize().height * 2));
+	japan->setTag(3);
+	hides.push_back(japan);
+	japan->addTouchEventListener(CC_CALLBACK_2(RoomScene::countryCallBack, this));
+	this->addChild(japan, 10);
 
-	auto c2 = Sprite::create("us.png");
-	c2->setPosition(Vec2(500, 500 - 30 * 3 - china->getContentSize().height * 3));
-	c2->setTag(4);
-	this->addChild(c2, 10);
+	auto russia = Button::create("russia.png");
+	russia->setPosition(Vec2(visibleSize.width / 2.0f , visibleSize.height / 2.0f - country_interval * 3 - china->getContentSize().height * 3));
+	hides.push_back(russia);
+	russia->addTouchEventListener(CC_CALLBACK_2(RoomScene::countryCallBack, this));
+	this->addChild(russia, 10);
 
 	//初始隐藏
-	for (auto i = hides.begin(); i != hides.end(); ++i)
-	{
-		auto sprite = getChildByTag(*i);
-		sprite->runAction(Hide::create());
+	for (auto &country : hides)
+	{		
+		country->runAction(Hide::create());		
 	}
 
 	auto pEditBox_name = EditBox::create(CCSizeMake(250,50), Scale9Sprite::create("text2.png"));
@@ -197,6 +165,65 @@ void RoomScene::editBoxReturn(EditBox *editBox)
 	}
 }
 
+//国家的回调函数
+void RoomScene::countryCallBack(Ref* pSender, Widget::TouchEventType type)
+{
+	
+	Button* button = static_cast<Button*>(pSender);
+	
+	if (type == Widget::TouchEventType::BEGAN)
+	{
+		log("countryCallBack");
+		bool hide = false;
+		for (auto &i : hides)
+		{
+			if (i == button)
+			{
+				hide = true; break;
+			}
+		}
+		//点击被选中就显示
+		if (hide == false)
+		{
+			log("hide==false");
+			for (auto i :hides)
+			{				
+				i->runAction(Show::create());
+			}
+		}
+		//点击滑块里的就改变
+		else
+		{
+			log("hide==true");
+			//交换位置
+			auto pos1 = button->getPosition();			
+			auto sprite = show[0];
+			button->setPosition(sprite->getPosition());
+			sprite->setPosition(pos1);
+
+			//交换标记
+			for (auto i =hides.begin();i!=hides.end();++i)
+			{
+				if (*i == button)
+				{
+					log("%d", i);
+					log("change flag ");
+					hides.erase(i);
+					hides.push_back(show[0]);
+					break;
+				}
+			}
+			
+			show[0]=button;
+
+			//隐藏
+			for (auto i :hides)
+			{
+				i->runAction(Hide::create());
+			}
+		}
+	}			
+}
 void RoomScene::editBoxTextChanged(EditBox *editBox, const std::string &text)
 {
 	CCLOG("EditBox_password changed");
@@ -211,91 +238,6 @@ void RoomScene::editBoxTextChanged(EditBox *editBox, const std::string &text)
 		
 	}
 	
-}
-
-//
-bool RoomScene::touchBegin(Touch* touch, Event* event)
-{
-	//获取被点击的国家
-	auto target = static_cast<Sprite*>(event->getCurrentTarget());
-	Vec2 locationInnode = target->convertToNodeSpace(touch->getLocation());
-	Size s = target->getContentSize();
-	Rect rect = Rect(0, 0, s.width, s.height);
-
-	log("choose%d", target->getTag());
-	bool hide = false;
-	for (auto i = hides.begin(); i != hides.end(); ++i)
-	{
-		if (*i == target->getTag())
-		{
-			hide = true; break;
-		}
-	}
-	log("%d", hide);
-
-	//点击被选中的国家就显示其他国家
-	if (hide == false)
-	{
-		for (auto i = hides.begin(); i != hides.end(); ++i)
-		{
-			auto sprite = getChildByTag(*i);
-			sprite->runAction(Show::create());
-		}
-	}
-	//点击滑块里的国家
-	else
-	{
-		//交换两个国家的位置
-		auto pos1 = target->getPosition();
-		target->setPosition(chosen_position);
-		auto sprite = getChildByTag(show[0]);
-		sprite->setPosition(pos1);
-
-		//交换两个国家的标记
-		for (auto i = hides.begin(); i != hides.end(); ++i)
-		{
-			if (*i == target->getTag())
-			{
-				*i = show.front();
-				break;
-			}
-		}
-		show.front() = target->getTag();
-
-		//隐藏滑块和国家
-		for (auto i = hides.begin(); i != hides.end(); ++i)
-		{
-			auto sprite = getChildByTag(*i);
-			sprite->runAction(Hide::create());
-		}
-
-	}
-	return true;
-}
-
-void RoomScene::touchMoved(Touch* touch, Event* event)
-{}
-
-void RoomScene::touchEnded(Touch* touch, Event* event)
-{}
-
-//Slider滑动事件回调函数
-void RoomScene::onChangedSlider(Ref* pSender, Slider::EventType type)
-{
-	if (type == Slider::EventType::ON_PERCENTAGE_CHANGED)
-	{
-		Slider* slider = dynamic_cast<Slider*>(pSender);
-		float percent = slider->getPercent() * 0.5;
-
-		//获取滑块里的国家进行移动
-		
-		for (auto i = hides.begin(),j=0; i != hides.end()-1; ++i,++j)
-		{
-			auto sprite = getChildByTag(*i);
-			sprite->runAction(MoveTo::create(0.1,Vec2(sprite->getPosition().x, sprite->getPosition().y + percent)));
-		}
-
-	}
 }
 
 void RoomScene::menuStartGameCallback(Ref* pSender)
