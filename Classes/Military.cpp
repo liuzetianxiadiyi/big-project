feadb0e1033d2642b2331f160e593113d710beb7
Akari <1137173537@qq.com>
2018-06-13 16:46:33 +0800

    Update Military.cpp

--- Classes/Military.cpp
+++ Classes/Military.cpp
@@ -16,16 +16,18 @@ Military* Military::create(string& filename)
 	return nullptr;
 };
 
-void Military::init(int _health_point, bool _dead, bool _Selected, Vec2 _position, float _destinationX,float _destinationY, Action* _move)
+void Military::init(int _health_point, bool _dead, bool _Selected, Vec2 _position, float _destinationX,float _destinationY, Action* _move,Vec2 _hpposition)
 {
 	name = _name;
 	health_point = _health_point;
+	max_hp= _health_point;
 	dead = _dead;
 	Selected = _Selected;
 	position = _position;
 	destinationX = _destinationX;
 	destinationY = _destinationY;
 	move = _move;
+	hpposition=_hpposition;
 }
 
 void Military::sethp(int damage)
@@ -35,7 +37,7 @@ void Military::sethp(int damage)
 
 void Military::setDestination(Vec2 v)
 {
-	destination = v;
+	position = v;
 }
 
 void Military::setdead()
@@ -48,12 +50,29 @@ void Military::setSelected(bool b)
 	Selected = b;
 }
 
+void Military::createBar(Sprite * a)
+{
+	Sprite *hpSprite = Sprite::create("hp.png");
+	auto hp = ProgressTimer::create(hpSprite);
+	hp->setType(ProgressTimer::Type::BAR);//条形
+	hp->setMidpoint(Point(0, 0));//血条起始点
+	hp->setBarChangeRate(Point(1, 0));//血条改变的是x方向
+	hp->setPosition(Vec2(32, 67));//血条相对精灵的位置
+	hp->setPercentage((health_point / max_hp) * 100);//设置值
+	a->addChild(hp, 1);//把血条绑定在精灵上
+}
+
 void Military::attack(Military* sprite)
 {
 	sprite->sethp(power);
 }
 
-int Military::gethp()const
+int Military::gethp()
 {
 	return health_point;
+}
+
+void Military::sethpDestination(Vec2 v)
+{
+	hpposition = v;
 }
