db0689213944b6280147f4f8fceee8a0b9eee9ef
Yama <958457134@qq.com>
2018-06-18 16:32:39 +0800

    @luozhen

---
+++ test/TestScene.cpp
@@ -0,0 +1,25 @@
+#include "TestScene.h"
+#include "Client.h"
+#include <string>
+#include "JsonParser.h"
+#include "GameData.h"
+
+Scene* TestScene::createScene()
+{
+	auto scene = Scene::create();
+	auto layer = Layer::create();
+	scene->addChild(layer);
+	return scene;
+}
+
+bool TestScene::init()
+{
+	Client* client = Client::getInstance();
+	string information = client->recv_Cli();
+	JsonParser* Json = JsonParser::createWithC_str(information.c_str());
+	Json->decode_WaitingData();
+
+	ValueMap temp = Json->getList()[0].asValueMap()[SWAITINGSCENEDATA].asValueMap();
+	log("AddRoom:%d", temp[ADDROOM].asInt());
+	log("Deleted:%d", temp[DELETED].asBool());
+}
---
+++ test/TestScene.h
@@ -0,0 +1,15 @@
+#ifndef __TESTSCENE_H__
+#define __TESTSCENE_H__
+
+#include "cocos2d.h"
+
+USING_NS_CC;
+
+class TestScene :public cocos2d::Layer
+{
+public:
+	static cocos2d::Scene* createScene();
+	virtual bool init();
+};
+
+#endif
