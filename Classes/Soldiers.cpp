6fadc15489777300d672e9aa6484234ff3e80537
zelotuo <37100615+yaya159177@users.noreply.github.com>
2018-06-22 07:12:04 +0800

    @liu@ji

--- Classes/Soldiers.cpp
+++ Classes/Soldiers.cpp
@@ -1,11 +1,11 @@
-﻿#include "Soldiers.h"
+#include "Soldiers.h"
 
 int Soldier::money = 500;
 int Dog::money = 300;
 int Engineer::money = 800;
 int Engineer::cure = 10;
 
-Soldier* Soldier::create(string & filename)
+Soldier* Soldier::create(const string  filename)
 {
 	Soldier* sprite = new Soldier();
 
@@ -15,7 +15,7 @@ Soldier* Soldier::create(string & filename)
 
 
 		sprite->init( 200,false,false, Vec2(300, 300),Vec2(0,0), MoveBy::create(sprite->getSpeed(), sprite->getDestination()));
-		 createBar(sprite);
+		createBar(sprite);
 		return sprite;
 	}
 
@@ -23,7 +23,7 @@ Soldier* Soldier::create(string & filename)
 	return nullptr;
 };
 
-Dog* Dog::create(string & filename)
+Dog* Dog::create(const string filename)
 {
 	Dog* sprite = new Dog();
 
@@ -41,7 +41,7 @@ Dog* Dog::create(string & filename)
 }
 
 
-Engineer* Engineer::create(string & filename)
+Engineer* Engineer::create(const string filename)
 {
 	Engineer* sprite = new Engineer();
 
@@ -58,15 +58,38 @@ Engineer* Engineer::create(string & filename)
 	return nullptr;
 };
 
-/*
-Sprite *hpSprite = Sprite::create("hp.png");
-auto hp = ProgressTimer::create(hpSprite);
-hp->setType(ProgressTimer::Type::BAR);//鏉″舰
-hp->setMidpoint(Point(0, 0));//琛€鏉¤捣濮嬬偣
-hp->setBarChangeRate(Point(1, 0));//琛€鏉℃敼鍙樼殑鏄痻鏂瑰悜
-hp->setPosition(Vec2(32, 67));//琛€鏉＄浉瀵圭簿鐏电殑浣嶇疆
-hp->setPercentage(100);//璁剧疆鍊?
+Tank* Tank::create(const string filename)
+{
+	Tank* sprite = new Tank();
 
-pSprite->addChild(hp, 1, 1);//鎶婅鏉＄粦瀹氬湪绮剧伒涓?
-*/
+	if (sprite->initWithFile(filename))
+	{
+		sprite->autorelease();
+
+		sprite->init(500, false, false, Vec2(300, 300), Vec2(300, 300), MoveBy::create(sprite->getSpeed(), sprite->getDestination()));
+		createBar(sprite);
+		return sprite;
+	}
+
+	CC_SAFE_DELETE(sprite);
+	return nullptr;
+};
+
+Miningcar* Miningcar::create(const string filename)
+{
+	Miningcar* sprite = new Miningcar();
+
+	if (sprite->initWithFile(filename))
+	{
+		sprite->autorelease();
+
+		sprite->init(500, false, false, Vec2(300, 300), Vec2(300, 300), MoveBy::create(sprite->getSpeed(), sprite->getDestination()));
+		createBar(sprite);
+		return sprite;
+	}
+
+	CC_SAFE_DELETE(sprite);
+	return nullptr;
+};
+
 
