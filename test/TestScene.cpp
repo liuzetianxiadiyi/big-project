cdede5d265e2122431b09d88070a7c8e73396880
Yama <958457134@qq.com>
2018-06-23 19:45:59 +0800

    @luozhen

--- test/TestScene.cpp
+++ test/TestScene.cpp
@@ -17,11 +17,12 @@ bool TestScene::init()
 	Client* client = Client::getInstance();
 	string information = client->recv_Cli();
 	log("%s", information);
-	/*JsonParser* Json = JsonParser::createWithC_str(information.c_str());
+	JsonParser* Json = JsonParser::createWithC_str(information.c_str());
 	Json->decode_WaitingData();
 
 	ValueMap temp = Json->getList()[0].asValueMap()[SWAITINGSCENEDATA].asValueMap();
 	log("AddRoom:%d", temp[ADDROOM].asInt());
-	log("Deleted:%d", temp[DELETED].asBool());*/
+	log("Deleted:%d", temp[DELETED].asBool());
 	client->send_Cli("");
+	return true;
 }
