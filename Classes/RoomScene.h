cb1f4b74cf0f93ae230befa90bcd9dd5a22e11b6
daydreamerer <38742662+daydreamerer@users.noreply.github.com>
2018-06-25 08:58:12 +0800

    Update RoomScene.h

--- Classes/RoomScene.h
+++ Classes/RoomScene.h
@@ -23,7 +23,6 @@ public:
 
 	void menuStartGameCallback(cocos2d::Ref* pSender);
 	void menuDeleteRoomCallback(cocos2d::Ref* pSender);
-	void messageCloseCallback(cocos2d::Ref* pSender);
 	void CountryButtonCallback(cocos2d::Ref* pSender);	
     	
 	//国家的button
@@ -34,12 +33,6 @@ public:
 	void countryCallBack(Ref* pSendre, Widget::TouchEventType type);
 	void confirmCallBack(Ref* pSendre, Widget::TouchEventType type);
 	
-	virtual void editBoxEditingDidBegin(cocos2d::extension::EditBox* editBox);
-	virtual void editBoxEditingDidEnd(cocos2d::extension::EditBox* editBox);
-	virtual void editBoxTextChanged(cocos2d::extension::EditBox* editBox, const std::string &text);
-	virtual void editBoxReturn(cocos2d::extension::EditBox *editBox);
-	
-	void onChangedSlider2(Ref* pSender, Slider::EventType type);
 
 	CREATE_FUNC(RoomScene);
 };
