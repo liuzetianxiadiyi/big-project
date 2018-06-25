a63cdc4faa2cdc94fb6340a8727dbe9ef1c5833a
daydreamerer <38742662+daydreamerer@users.noreply.github.com>
2018-06-25 20:06:23 +0800

    Update WaitingScene.cpp

--- Classes/WaitingScene.cpp
+++ Classes/WaitingScene.cpp
@@ -2,26 +2,24 @@
 #include "WaitingScene.h"
 #include "RoomScene.h"
 #include "GameData.h"
-#include "Client.h"
 #include "JsonParser.h"
 #include "enJsonParser.h"
-#include "SystemHeader.h"
-#include <mutex>
-
+#include "Information.h"
 #include <string>
-
+#include"Client.h"
+using std::string;
 #define ROOMBUTTONWIDTH 60
 
+extern Information information;
 //待完善
-int WaitingScene::room_nums = 0;
+
 int WaitingScene::SelectedRoomTag = -1;
 bool WaitingScene::replace = false;
-mutex mtx;
 
 Scene* WaitingScene::createScene()
 {
 	auto scene = Scene::create();
-	auto layer = Layer::create();
+	auto layer = WaitingScene::create();
 	scene->addChild(layer);
 	return scene;
 }
@@ -34,42 +32,42 @@ bool WaitingScene::init()
 	}
 
 	replace = false;
-
+	room_nums = 0;
 	auto visibleSize = Director::getInstance()->getVisibleSize();
 	Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
-	thread roomThread(this->roomDataThread);
-	roomThread.detach();
+	//thread roomThread([&] {this->roomDataThread(); });
+	//roomThread.detach();
 
 	auto EnterItem = MenuItemImage::create(
 		"EnterNormal.png",
 		"EnterSelected.png",
 		CC_CALLBACK_1(WaitingScene::menuEnterCallback, this)
 	);
-	EnterItem->setPosition(Vec2(visibleSize.width / 2, visibleSize.height / 2 - 100));		//美工了解一下
+	EnterItem->setPosition(Vec2(125 + visibleSize.width / 2, visibleSize.height / 2 + 60));	//美工了解一下
 
 	auto createRoomItem = MenuItemImage::create(
 		"createRoomNormal.png",
 		"createRoomSelected.png",
-		CC_CALLBACK_1(WaitingScene::createRoomCallback, this)
+		CC_CALLBACK_1(WaitingScene::menuReturnCallback, this)
 	);
-	createRoomItem->setPosition(Vec2(visibleSize.width / 2, visibleSize.height / 2 - 100));
+	createRoomItem->setPosition(Vec2(125 + visibleSize.width / 2, visibleSize.height / 2));
 
 	auto ReturnItem = MenuItemImage::create(
 		"ReturnNormal.png",
 		"ReturnSelected.png",
 		CC_CALLBACK_1(WaitingScene::menuReturnCallback, this)
 	);
-	ReturnItem->setPosition(Vec2(visibleSize.width / 2, visibleSize.height / 2 - 100));
+	ReturnItem->setPosition(Vec2(125 + visibleSize.width / 2, visibleSize.height / 2 - 60));
 
 	auto menu = Menu::create(EnterItem, createRoomItem, ReturnItem, NULL);
 	menu->setPosition(Vec2::ZERO);
 	this->addChild(menu, 1);
 
-	auto sprite = Sprite::create(".png");
+	auto sprite = Sprite::create("accont-back.png");
 	sprite->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
 	this->addChild(sprite, 0);
-	Slider* slider = Slider::create();
+Slider* slider = Slider::create();
 	//加载滑杆纹理
 	slider->loadBarTexture("sliderTrack.png");
 	//加载滑块按钮纹理
@@ -81,66 +79,87 @@ bool WaitingScene::init()
 	slider->setRotation(90);
 
 	slider->setPosition(Vec2(visibleSize.width / 2.0f + 60, visibleSize.height / 2.0f));
-	slider->addEventListener(CC_CALLBACK_2(WaitingScene::onChangedSlider, this));
+	//slider->addEventListener(CC_CALLBACK_2(WaitingScene::onChangedSlider, this));
 	this->addChild(slider, 1);
+	//room_nums = 10;
+	for (int i = 0; i < room_nums; ++i)
+	{
+		auto visibleSize = Director::getInstance()->getVisibleSize();
+		Vec2 origin = Director::getInstance()->getVisibleOrigin();
+		auto roomButton = Button::create("room.png", "roomHighlight.png");
+		//美工
+		roomButton->setScale9Enabled(true);
+		roomButton->setTitleText(to_string(i));
+		roomButton->setTitleFontSize(35);
+		roomButton->setContentSize(Size(100, 20));
+		roomButton->setPosition(Vec2(visibleSize.width - 100 * i, 50));
+
+		roomButton->addClickEventListener(CC_CALLBACK_1(WaitingScene::clickRoomcallback, this));
+
+		roomButton->setTag(i);
+		this->addChild(roomButton, 2);
+	}
 
 	return true;
 }
 
-void WaitingScene::roomDataThread()
-{
-	mtx.lock();
-	while (!replace)
-	{
-		Client* client = Client::getInstance();
-		string information;
-		if ((information=client->recv_Cli())!="")
-		{
-			JsonParser* json = JsonParser::createWithC_str(information.c_str());
-			ValueMap DataMap = json->decode_WaitingData();
-			if (DataMap.find(SWAITINGSCENEDATA) != DataMap.end())
-			{
-				ValueMap Data = DataMap[SWAITINGSCENEDATA].asValueMap();
-
-				room_nums = Data[ADDROOM].asInt();
-
-				ValueVector room_tag = Data[ROOMLABEL].asValueVector();
-
-				bool isDeleted = Data[DELETED].asBool();
-
-				if (isDeleted)
-				{
-					ValueVector DeletedRoom = Data[DELETEDROOM].asValueVector();
-					for (int i = 0; i < DeletedRoom.size(); ++i)
-					{
-						removeChildByTag(DeletedRoom[i].asInt());
-					}
-				}
-
-				for (int i = 0; i < room_nums; ++i)
-				{
-					auto visibleSize = Director::getInstance()->getVisibleSize();
-					Vec2 origin = Director::getInstance()->getVisibleOrigin();
-					auto roomButton = Button::create("room.png", "roomHighlight.png");
-					//美工
-					roomButton->setScale9Enabled(true);
-					roomButton->setTitleText(to_string(room_tag.at(i).asInt()));
-					roomButton->setTitleFontSize(35);
-					roomButton->setContentSize(Size(100, 20));
-					roomButton->setPosition(Vec2(visibleSize.width - 100, 50));
-
-					roomButton->addClickEventListener(CC_CALLBACK_1(WaitingScene::clickRoomcallback, this));
-
-					roomButton->setTag(i);
-					this->addChild(roomButton, 2);
-				}
-			}
-
-		}
-		Sleep(2*TIME_LAG);
-	}
-	mtx.unlock();
-}
+//void WaitingScene::roomDataThread()
+//{
+//	//rmtx.lock();
+//	while (!replace)
+//	{
+//		Client* client = Client::getInstance();
+//		//if (client->recv_Cli())
+//		
+//			//rmtx.lock();
+//
+//			JsonParser* json = JsonParser::createWithC_str(information.getRecvBuf().c_str());
+//			json->decode_WaitingData();
+//			ValueMap DataMap = json->getList().at(0).asValueMap();		//0 is default position
+//			if (DataMap.find(SWAITINGSCENEDATA) != DataMap.end())
+//			{
+//				ValueMap Data = DataMap[SWAITINGSCENEDATA].asValueMap();
+//
+//				room_nums = Data[ADDROOM].asInt();
+//
+//				ValueVector room_tag = Data[ROOMLABEL].asValueVector();
+//
+//				bool isDeleted = Data[DELETED].asBool();
+//
+//				if (isDeleted)
+//				{
+//					ValueVector DeletedRoom = Data[DELETEDROOM].asValueVector();
+//					for (int i = 0; i < DeletedRoom.size(); ++i)
+//					{
+//						removeChildByTag(DeletedRoom[i].asInt());
+//					}
+//				}
+//
+//				for (int i = 0; i < room_nums; ++i)
+//				{
+//					auto visibleSize = Director::getInstance()->getVisibleSize();
+//					Vec2 origin = Director::getInstance()->getVisibleOrigin();
+//					auto roomButton = Button::create("room.png", "roomHighlight.png");
+//					//美工
+//					roomButton->setScale9Enabled(true);
+//					roomButton->setTitleText(to_string(room_tag.at(i).asInt()));
+//					roomButton->setTitleFontSize(35);
+//					roomButton->setContentSize(Size(100, 20));
+//					roomButton->setPosition(Vec2(visibleSize.width - 100, 50));
+//
+//					roomButton->addClickEventListener(CC_CALLBACK_1(WaitingScene::clickRoomcallback, this));
+//
+//					roomButton->setTag(i);
+//					this->addChild(roomButton, 2);
+//				}
+//			}
+//
+//			//rmtx.unlock();
+//		
+//		Sleep(2*TIME_LAG);
+//	}
+//	//rmtx.unlock();
+//}
 
 void WaitingScene::menuEnterCallback(Ref* pSender)
 {
@@ -151,7 +170,7 @@ void WaitingScene::menuEnterCallback(Ref* pSender)
 		//send room_tag and player of this room message
 		ValueVector plistdata = GameData::WaitingData(false, SelectedRoomTag, defaults->getStringForKey(PLAYERNAME));
 
-		mtx.lock();
+		//rmtx.lock();
 		replace = true;
 
 		enJsonParser* enJson = enJsonParser::createWithArray(plistdata);
@@ -159,7 +178,7 @@ void WaitingScene::menuEnterCallback(Ref* pSender)
 		Client* client = Client::getInstance();
 		client->send_Cli(sendbuf);
 
-		mtx.unlock();
+		//rmtx.unlock();
 		//Scene changes
 		auto scene = RoomScene::createScene();
 		auto reScene = TransitionJumpZoom::create(1.0f, scene);
@@ -173,11 +192,11 @@ void WaitingScene::menuEnterCallback(Ref* pSender)
 
 void WaitingScene::createRoomCallback(Ref* pSender)
 {
-	mtx.lock();
+	//rmtx.lock();
 
 	replace = true;
 
-	mtx.unlock();
+	//rmtx.unlock();
 
 	UserDefault* defaults = UserDefault::getInstance();
 	defaults->setBoolForKey(OWNER, true);
@@ -188,12 +207,10 @@ void WaitingScene::createRoomCallback(Ref* pSender)
 	enJsonParser* enJson = enJsonParser::createWithArray(plistdata);
 	string sendbuf = enJson->encode_WaitingRoomData();
 
-	mtx.lock();
-
 	Client* client = Client::getInstance();
 	client->send_Cli(sendbuf);
 
-	mtx.unlock();
+	//rmtx.unlock();
 
 	auto scene = RoomScene::createScene(); 
 	auto reScene = TransitionJumpZoom::create(1.0f, scene);
@@ -206,9 +223,8 @@ void WaitingScene::createRoomCallback(Ref* pSender)
 
 void WaitingScene::menuReturnCallback(Ref* pSender)
 {
-	mtx.lock();
+	//rmtx.lock();
 	replace = true;
-	mtx.unlock();
 
 	auto scene = HelloWorld::createScene();
 	auto reScene = TransitionJumpZoom::create(1.0f, scene);
@@ -225,7 +241,7 @@ void WaitingScene::clickRoomcallback(Ref* pSender)
 
 	if (SelectedRoom->isHighlighted())
 	{
-		menuEnterCallback(pSender);
+		/*menuEnterCallback(pSender);*/
 	}
 	else
 	{
